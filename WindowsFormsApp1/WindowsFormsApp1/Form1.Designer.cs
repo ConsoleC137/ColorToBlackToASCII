784d781f53aaf85f5f7acaab46c49968a0325840
Console <66591395+ConsoleC137@users.noreply.github.com>
2021-10-30 17:27:57 +0700

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.BlackCheckBox = new System.Windows.Forms.CheckBox();
+            this.NegativeCheckBox = new System.Windows.Forms.CheckBox();
+            this.ASCIICheckBox = new System.Windows.Forms.CheckBox();
+            this.Hide = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            this.labelN = new System.Windows.Forms.Label();
+            this.labelA = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.labelHeight = new System.Windows.Forms.Label();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(12, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(256, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Преобразовать изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // BlackCheckBox
+            // 
+            this.BlackCheckBox.AutoSize = true;
+            this.BlackCheckBox.Location = new System.Drawing.Point(12, 88);
+            this.BlackCheckBox.Name = "BlackCheckBox";
+            this.BlackCheckBox.Size = new System.Drawing.Size(187, 18);
+            this.BlackCheckBox.TabIndex = 1;
+            this.BlackCheckBox.Text = "Чёрно-белое изображение";
+            this.BlackCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // NegativeCheckBox
+            // 
+            this.NegativeCheckBox.AutoSize = true;
+            this.NegativeCheckBox.Location = new System.Drawing.Point(12, 64);
+            this.NegativeCheckBox.Name = "NegativeCheckBox";
+            this.NegativeCheckBox.Size = new System.Drawing.Size(180, 18);
+            this.NegativeCheckBox.TabIndex = 2;
+            this.NegativeCheckBox.Text = "Негативное изображение";
+            this.NegativeCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ASCIICheckBox
+            // 
+            this.ASCIICheckBox.AutoSize = true;
+            this.ASCIICheckBox.Location = new System.Drawing.Point(12, 112);
+            this.ASCIICheckBox.Name = "ASCIICheckBox";
+            this.ASCIICheckBox.Size = new System.Drawing.Size(145, 18);
+            this.ASCIICheckBox.TabIndex = 3;
+            this.ASCIICheckBox.Text = "ASCII изображение";
+            this.ASCIICheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Hide
+            // 
+            this.Hide.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Hide.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Hide.FlatAppearance.BorderSize = 0;
+            this.Hide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Hide.Location = new System.Drawing.Point(205, 0);
+            this.Hide.Margin = new System.Windows.Forms.Padding(0);
+            this.Hide.Name = "Hide";
+            this.Hide.Size = new System.Drawing.Size(40, 25);
+            this.Hide.TabIndex = 5;
+            this.Hide.Text = "_";
+            this.Hide.UseVisualStyleBackColor = false;
+            this.Hide.Click += new System.EventHandler(this.Hide_Click);
+            // 
+            // Close
+            // 
+            this.Close.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Close.FlatAppearance.BorderSize = 0;
+            this.Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Close.Location = new System.Drawing.Point(245, 0);
+            this.Close.Margin = new System.Windows.Forms.Padding(0);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(40, 25);
+            this.Close.TabIndex = 5;
+            this.Close.Text = "X";
+            this.Close.UseVisualStyleBackColor = false;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 19);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ColorToBlackToASCII";
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.Location = new System.Drawing.Point(198, 68);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(21, 14);
+            this.labelB.TabIndex = 7;
+            this.labelB.Text = "0%";
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Location = new System.Drawing.Point(205, 89);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(21, 14);
+            this.labelN.TabIndex = 8;
+            this.labelN.Text = "0%";
+            // 
+            // labelA
+            // 
+            this.labelA.AutoSize = true;
+            this.labelA.Location = new System.Drawing.Point(163, 113);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(21, 14);
+            this.labelA.TabIndex = 9;
+            this.labelA.Text = "0%";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 138);
+            this.progressBar1.MarqueeAnimationSpeed = 1;
+            this.progressBar1.Maximum = 10;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(256, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 10;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(12, 167);
+            this.progressBar2.Maximum = 10;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(256, 23);
+            this.progressBar2.Step = 1;
+            this.progressBar2.TabIndex = 11;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(13, 196);
+            this.progressBar3.Maximum = 10;
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(256, 23);
+            this.progressBar3.TabIndex = 12;
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(64, 36);
+            this.textBoxWidth.MaxLength = 4;
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(33, 22);
+            this.textBoxWidth.TabIndex = 13;
+            this.textBoxWidth.Text = "520";
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.Location = new System.Drawing.Point(12, 39);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(49, 14);
+            this.labelWidth.TabIndex = 14;
+            this.labelWidth.Text = "Ширина";
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.Location = new System.Drawing.Point(124, 39);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(49, 14);
+            this.labelHeight.TabIndex = 15;
+            this.labelHeight.Text = "Высота";
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(175, 36);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(33, 22);
+            this.textBoxHeight.TabIndex = 16;
+            this.textBoxHeight.Text = "2.98";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(280, 280);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.labelHeight);
+            this.Controls.Add(this.labelWidth);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelA);
+            this.Controls.Add(this.labelN);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Hide);
+            this.Controls.Add(this.ASCIICheckBox);
+            this.Controls.Add(this.NegativeCheckBox);
+            this.Controls.Add(this.BlackCheckBox);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.85D;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ColorToBlackToASCII";
+            this.TopMost = true;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox BlackCheckBox;
+        private System.Windows.Forms.CheckBox NegativeCheckBox;
+        private System.Windows.Forms.CheckBox ASCIICheckBox;
+        private System.Windows.Forms.Button Hide;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.Label labelWidth;
+        private System.Windows.Forms.Label labelHeight;
+        private System.Windows.Forms.TextBox textBoxHeight;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public Form1()
+        {
+            InitializeComponent();
+            OpenFileDialog = new OpenFileDialog()
+            {
+                Filter = "Image | *.bmp; *.png; *.jpg; *.jpeg",
+                Title = "Какое изображение преобразовать?"
+            };
+        }
+
+        private void Initze()
+        {
+            SaveFileDialogBlack = new SaveFileDialog()
+            {
+                Filter = "Image | *.bmp; *.png; *.jpg; *.jpeg",
+                Title = "Куда сохранить чёрно-белое изображение?",
+                DefaultExt = Ext,
+                FileName = NameF + "_Black" + Ext
+            };
+
+            SaveFileDialogNegative = new SaveFileDialog()
+            {
+                Filter = "Image | *.bmp; *.png; *.jpg; *.jpeg",
+                Title = "Куда сохранить негативное изображение?",
+                DefaultExt = Ext,
+                FileName = NameF + "_Negative" + Ext
+            };
+
+            SaveFileDialogASCII = new SaveFileDialog()
+            {
+                Filter = "Text | *.txt;",
+                Title = "Куда сохранить ASCII изображение?",
+                DefaultExt = ".txt",
+                FileName = NameF + "_ASCII" + ".txt"
+            };
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            labelB.Text = "0%";
+            labelN.Text = "0%";
+            labelA.Text = "0%";
+            progressBar1.Value = 0;
+            progressBar2.Value = 0;
+            progressBar3.Value = 0;
+
+            MAX_WIDTH = Convert.ToInt32(textBoxWidth.Text);
+
+            if (textBoxHeight.Text.Contains("."))
+                textBoxHeight.Text = textBoxHeight.Text.Replace('.', ',');
+
+            K_HEIGHT = Convert.ToDouble(textBoxHeight.Text);
+
+            IsRunning = false;
+            while (!IsRunning)
+            {
+                if (BlackCheckBox.Checked || NegativeCheckBox.Checked || ASCIICheckBox.Checked)
+                {
+                    if (OpenFileDialog.ShowDialog()==DialogResult.OK)
+                    {
+                        if (OpenFileDialog.FileName != "")
+                        {
+                            NameExt(OpenFileDialog.FileName);
+                            Initze();
+                        }
+                        else
+                            break;
+
+                        Bitmap bitmap = new Bitmap(Image.FromFile(OpenFileDialog.FileName));
+                        Bitmap bitmapClone = (Bitmap)bitmap.Clone();
+                        Bitmap bitmapClone1 = (Bitmap)bitmap.Clone();
+
+                        if (BlackCheckBox.Checked && SaveFileDialogBlack.ShowDialog() == DialogResult.OK)
+                        {
+                            BlackFunc(bitmap, SaveFileDialogBlack.FileName);
+                        }
+                        if (NegativeCheckBox.Checked && SaveFileDialogNegative.ShowDialog() == DialogResult.OK)
+                        {
+                            NegativeFunc(bitmapClone, SaveFileDialogNegative.FileName);
+                        }
+                        if (ASCIICheckBox.Checked && SaveFileDialogASCII.ShowDialog() == DialogResult.OK)
+                        {
+                            ASCIIFunc(bitmapClone1, SaveFileDialogASCII.FileName);
+                        }
+                    }
+                }
+                else
+                    MessageBox.Show("Выбирете режим!", "Ошибка");
+
+                IsRunning = true;
+                GC.Collect();
+            }
+        }
+        private int MAX_WIDTH { get; set; }
+        private double K_HEIGHT { get; set; }
+        private const double K = 174.496644295302;
+        private static char[] Symbols = { '@', '#', '&', '$', '%', '?', '/', ':', ',', '.' };
+        private static string Ext { get; set; }
+        private static string NameF { get; set; }
+        private static OpenFileDialog OpenFileDialog { get; set; }
+        private static SaveFileDialog SaveFileDialogBlack { get; set; }
+        private SaveFileDialog SaveFileDialogNegative { get; set; }
+        private bool IsRunning { get; set; }
+        public SaveFileDialog SaveFileDialogASCII { get; private set; }
+
+        private delegate void Prog(int x);
+        private delegate void Prog1();
+
+        private void NegativeFunc(Bitmap invImage, string path)
+        {
+            new Thread(() =>
+            {
+                Prog prog1 = (x) =>
+                {
+                    labelN.Text = $"{x}%";
+                };
+
+                Prog1 prog1End = () =>
+                {
+                    labelN.Text = "100%";
+                };
+
+                Prog1 bar1 = () =>
+                { progressBar2.PerformStep(); progressBar2.Value--; progressBar2.PerformStep(); };
+
+                Prog1 bar1end = () =>
+                { progressBar2.Value = progressBar2.Maximum; };
+                int x = 0;
+                int counter = 0;
+                int hundred = invImage.Height / 100;
+                int counterbar = 0;
+                int ten = invImage.Height / 10;
+                Color inv;
+                for (int i = 0; i < invImage.Height; i++)
+                {
+                    counter++;
+                    counterbar++;
+                    for (int j = 0; j < invImage.Width; j++)
+                    {
+                        inv = invImage.GetPixel(j, i);
+
+                        invImage.SetPixel(j, i, Color.FromArgb(255 - inv.R, 255 - inv.G, 255 - inv.B));
+                    }
+                    if (counterbar == ten)
+                    {
+                        progressBar2.Invoke(bar1);
+                        counterbar = 0;
+                    }
+                    if (counter == hundred)
+                    {
+                        x++;
+                        labelN.Invoke(prog1, x);
+                        counter = 0;
+                    }
+                }
+                progressBar2.Invoke(bar1end);
+                labelN.Invoke(prog1End);
+                invImage.Save(path);
+            }).Start();
+            GC.Collect();
+
+        }
+        private void BlackFunc(Bitmap imageBlack, string path)
+        {
+            new Thread(() =>
+            {
+                Prog prog1 = (x) =>
+                { labelB.Text = $"{x}%"; };
+
+                Prog1 prog1End = () =>
+                { labelB.Text = "100%"; };
+
+                Prog1 bar1 = () =>
+                { progressBar1.PerformStep(); progressBar1.Value--; progressBar1.PerformStep(); };
+
+                Prog1 bar1end = () =>
+                { progressBar1.Value = progressBar1.Maximum; };
+                int x = 0;
+                int counter = 0;
+                int hundred = imageBlack.Height / 100;
+                int counterbar = 0;
+                int ten = imageBlack.Height / 10;
+                Color avgC;
+                int avg;
+                for (int i = 0; i < imageBlack.Height; i++)
+                {
+                    counter++;
+                    counterbar++;
+                    for (int j = 0; j < imageBlack.Width; j++)
+                    {
+                        avgC = imageBlack.GetPixel(j, i);
+                        avg = (avgC.R + avgC.G + avgC.B) / 3;
+                        imageBlack.SetPixel(j, i, Color.FromArgb(avg, avg, avg));
+                    }
+                    if (counterbar == ten)
+                    {
+                        progressBar1.Invoke(bar1);
+                        counterbar = 0;
+                    }
+                    if (counter == hundred)
+                    {
+                        x++;
+                        labelB.Invoke(prog1, x);
+                        counter = 0;
+                    }
+                }
+                progressBar1.Invoke(bar1end);
+                labelB.Invoke(prog1End);
+                imageBlack.Save(path);
+
+
+            }).Start();
+            GC.Collect();
+        }
+        private void ASCIIFunc(Bitmap ResizeImage7, string path)
+        {
+            new Thread(() =>
+            {
+                Bitmap ResizeImage = ResizeBitmap(ResizeImage7);
+                Prog prog1 = (x) =>
+                {
+                    labelA.Text = $"{x}%";
+                };
+
+                Prog1 prog1End = () =>
+                {
+                    labelA.Text = "100%";
+                };
+
+                Prog1 bar1 = () =>
+                { progressBar3.PerformStep(); progressBar3.Value--; progressBar3.PerformStep(); };
+
+                Prog1 bar1end = () =>
+                { progressBar3.Value = progressBar3.Maximum; };
+                int index;
+                int avg1;
+                int x = 0;
+                int counter = 0;
+                int hundred = ResizeImage.Height / 100;
+                int counterbar = 0;
+                int ten = ResizeImage.Height / 10;
+                using FileStream file = new FileStream(path, FileMode.Create);
+                using StreamWriter streamWriter = new StreamWriter(file);
+                for (int i = 0; i < ResizeImage.Height; i++)
+                {
+                    for (int j = 0; j < ResizeImage.Width; j++)
+                    {
+                        avg1 = (ResizeImage.GetPixel(j, i).R
+                                     + ResizeImage.GetPixel(j, i).G
+                                     + ResizeImage.GetPixel(j, i).B) / 3;
+                        ResizeImage.SetPixel(j, i, Color.FromArgb(avg1, avg1, avg1));
+
+                        index = (int)Map(avg1, 0, 255, 0, Symbols.Length - 1);
+
+                        streamWriter.Write(Symbols[index]);
+                    }
+                    streamWriter.Write("\n");
+                    if (counterbar == ten)
+                    {
+                        progressBar3.Invoke(bar1);
+                        counterbar = 0;
+                    }
+                    if (counter == hundred)
+                    {
+                        x++;
+                        labelA.Invoke(prog1, x);
+                        counter = 0;
+                    }
+                }
+                progressBar3.Invoke(bar1end);
+                labelA.Invoke(prog1End);
+            }).Start();
+            GC.Collect();
+
+        }
+        public static void NameExt(string path)
+        {
+            string[] exPath = path.Split('\\');
+            string[] fileName = exPath[exPath.Length - 1].Split('.');
+            Ext = "." + fileName[1];
+            NameF = fileName[0];
+        }
+        private static float Map(float valueToMap,
+            float start1,
+            float stop1,
+            float start2,
+            float stop2)
+        {
+            return ((valueToMap - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+        private Bitmap ResizeBitmap(Bitmap bitmap)
+        {
+            var maxWidth = MAX_WIDTH;
+            var newHeight = bitmap.Height / K_HEIGHT * maxWidth / bitmap.Width;
+            if (bitmap.Width > maxWidth || bitmap.Height > newHeight)
+            {
+                bitmap = new Bitmap(bitmap, new Size(maxWidth, (int)newHeight));
+            }
+            return bitmap;
+        }
+
+        private void Hide_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+    }
+}
